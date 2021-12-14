624f44078dc24f127c0eda557916902990e7f481
Sonny <sonny.luscap@gmail.com>
2021-12-15 00:52:49 +0100

    ajout Stripe

--- Job-Portal/app/Models/User.php
+++ Job-Portal/app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Cashier\Billable;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Billable;
 
     /**
      * The attributes that are mass assignable.
@@ -32,6 +34,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    // Un utilisateur peut avoir plusieurs listing associés
+    public function listing() {
+        return $this->hasMany(related: Listing::class); 
+    }
 
     /**
      * The attributes that should be cast.
